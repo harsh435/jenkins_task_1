8a6ab56a17e5b767033db16e64848854266ce711
harsh435 <harshsangwan1105@gmail.com>
2023-11-23 16:17:15 +0530

    buildscript added

---
+++ buildscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker ps -a | grep 'assignment1314' > output
+chmod 777 output
+if [ -s output ]
+then
+        sudo docker stop myimage
+        sudo docker rm myimage
+        docker build -t assignment1314 .
+        docker run --name myimage -d -p 8081:80 assignment1314
+else
+
+        docker build -t assignment1314 .
+        docker run --name myimage -d -p 8081:80 assignment1314
+
+fi
+rm -r output
