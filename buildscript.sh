77475c2d43bbb466c3a4d93f8ba30b9262c9ff23
harsh435 <56024045+harsh435@users.noreply.github.com>
2023-11-23 21:43:43 +0530

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 docker ps -a | grep 'assignment1314' > output
 chmod 777 output
-if [ -S output ]
+if [ -s output ]
 then
-        sudo docker stop myimage
-        sudo docker rm myimage
-	sudo docker rmi myimage
+        docker stop myimage
+        docker rm myimage
+	docker rmi myimage
         docker build -t assignment1314 .
         docker run --name myimage -d -p 8081:80 assignment1314
 else
         docker build -t assignment1314 .
         docker run --name myimage -d -p 8081:80 assignment1314
 fi
-sudo rm -r output
+rm -r output
