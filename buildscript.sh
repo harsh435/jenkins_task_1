0fb256ec591a8288036dda6e25ccf144e9d86a5b
harsh435 <56024045+harsh435@users.noreply.github.com>
2023-11-23 22:03:35 +0530

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #docker ps -a | grep 'assignment1314' > output
 #chmod 777 output
-container=docker ps -aq | xargs
-docker stop $container
-docker rm $container
-docker rmi -f $(docker images -aq)
+#container=docker ps -aq | xargs
+#docker stop $container
+#docker rm $container
+#docker rmi -f $(docker images -aq)
+apt-get install psmisc
+fuser -k 9042/tcp
 docker build -t assignment1314 .
 docker run -d -p 8081:80 assignment1314
 #else
